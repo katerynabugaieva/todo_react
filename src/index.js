6c0836a0aa696232bbcc3cc000e5f1b01d878bb7
Kateryna Bugaieva <sonnensmile@gmail.com>
2019-07-19 18:18:58 +0200

    first todo elements

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-//const el = React.createElement('h1', null, 'First element');
-const el = <h1>First react element</h1>
+const el = (
+<div>
+    <h1>My TODO List</h1>
+    <input placeholder="search"/>
+    <ul>
+        <li>Learn react</li>
+        <li>Build awesome app </li>
+    </ul>
+</div>
+)
 ReactDOM.render( el, document.getElementById('root'));
 
