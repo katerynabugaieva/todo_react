cdf69ec9480aab4d27a9a0922806f152494f1418
Kateryna Bugaieva <sonnensmile@gmail.com>
2019-07-19 18:14:50 +0200

    first react element

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-console.log("hello")
+import React from "react";
+import ReactDOM from "react-dom";
+
+//const el = React.createElement('h1', null, 'First element');
+const el = <h1>First react element</h1>
+ReactDOM.render( el, document.getElementById('root'));
+
