a8e69fe75398512decd7ef838058e38e0ea4d48d
Kateryna Bugaieva <kateryna.bugaieva@xs-nb-10.local>
2019-07-22 12:56:32 +0200

    app-component as separate component

---
+++ src/component/app/app.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import AppHeader from '../app-header'
+import SearchPanel from '../search-panel'
+import ItemStatusFilter from '../item-status-filter'
+import TodoList from '../todo-list'
+
+const App = () =>{
+    const todoData = [
+        {id: 0, label: 'Drink more coffee', important: false},
+        {id: 1, label: 'Make awesome app', important: true},
+        {id: 2, label: 'Have lunch', important: false}
+        
+    ]
+    return (
+        <div className="todo-app">
+        <AppHeader toDo={1} done={3} />
+        <div className="top-panel d-flex">
+          <SearchPanel />
+          <ItemStatusFilter />
+        </div>
+  
+        <TodoList todos={todoData} />
+      </div>
+)}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,30 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import AppHeader from './component/app-header'
-import SearchPanel from './component/search-panel/search-panel'
-import TodoList from './component/todo-list/todo-list'
-import ItemStatusFilter from './component/item-status-filter'
+import App from './component/app'
 import './index.css'
-
-
-const App = () =>{
-    const todoData = [
-        {id: 0, label: 'Drink more coffee', important: false},
-        {id: 1, label: 'Make awesome app', important: true},
-        {id: 2, label: 'Have lunch', important: false}
-        
-    ]
-    return (
-        <div className="todo-app">
-        <AppHeader toDo={1} done={3} />
-        <div className="top-panel d-flex">
-          <SearchPanel />
-          <ItemStatusFilter />
-        </div>
-  
-        <TodoList todos={todoData} />
-      </div>
-)}
 
 ReactDOM.render(<App/>, document.getElementById('root'));
 
