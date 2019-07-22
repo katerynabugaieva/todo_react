d3d82a0cbb6f19d8f24895590c2dee8ef079d083
Kateryna Bugaieva <kateryna.bugaieva@xs-nb-10.local>
2019-07-22 11:06:51 +0200

    spread objects

--- src/component/todo-list.js
+++ src/component/todo-list.js
@@ -5,16 +5,13 @@ const TodoList = ({ todos }) => {
     const elements = todos.map((item) => {
         return  (
         <li>
-            <TodoListItem 
-                label={item.label}
-                important= {item.important}
-        />
+            <TodoListItem {...item}/>
         </li>)
     })
     return (
-<ul>
-        {elements}
-    </ul>
+        <ul>
+            { elements }
+        </ul>
     )
 }
 export default TodoList
