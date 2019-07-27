8fae9b3414bb554bc6a28b2a3729aaa6de1a4c89
Kateryna Bugaieva <kateryna.bugaieva@xs-nb-10.local>
2019-07-27 20:03:48 +0200

    return newArray

--- src/component/app/app.js
+++ src/component/app/app.js
@@ -1,16 +1,30 @@
-import React from 'react'
+import React, {Component} from 'react'
 import AppHeader from '../app-header'
 import SearchPanel from '../search-panel'
 import ItemStatusFilter from '../item-status-filter'
 import TodoList from '../todo-list'
 
-const App = () =>{
-    const todoData = [
+export default class App extends Component {
+  state ={  
+    todoData : [
         {id: 0, label: 'Drink more coffee', important: false},
         {id: 1, label: 'Make awesome app', important: true},
-        {id: 2, label: 'Have lunch', important: false}
-        
+        {id: 2, label: 'Have lunch', important: false} 
     ]
+  }
+  deleteItem = ( id )=>{
+    this.setState(({ todoData }) => {
+      const idx = todoData.findIndex((el) => el.id === id)
+      const newArray = [
+        ...todoData.slice(0, idx),
+        ...todoData.slice(idx +1 )
+      ]
+      return {
+        todoData: newArray
+      }
+    })
+  }
+  render(){
     return (
         <div className="todo-app">
         <AppHeader toDo={1} done={3} />
@@ -20,9 +34,7 @@ const App = () =>{
         </div>
   
         <TodoList 
-        todos={todoData}
-        onDeleted = {(id)=> console.log(id)}/>
+        todos={this.state.todoData}
+        onDeleted = {this.deleteItem}/>
       </div>
-)}
-
-export default App;
+    )}}
